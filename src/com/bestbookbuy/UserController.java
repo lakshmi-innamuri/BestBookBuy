e6c990337fd40c0b1a6f8c43a65284c89cec6fca
lakshmi <lakshmi@10.0.0.27>
2016-04-18 12:04:03 -0400

    dd updates

--- src/com/bestbookbuy/UserController.java
+++ src/com/bestbookbuy/UserController.java
@@ -17,7 +17,7 @@ import org.springframework.ui.ModelMap;
  *
  */
 
-@Controller
+@Controller("BestBookBuy")
 public class UserController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
